f7c9426b7bb76fe7145c928e1ad9743cb13464be
Sara Gray <sara@multiply.ai>
2023-03-16 08:12:39 +0000

    feat: initial app setup for creating an npm package

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,22 @@
 'use client'
 
-import { useState } from 'react'
+import { Badge, Button } from '@/lib/components'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const LOCAL = process.env.NEXT_PUBLIC_CCC_LOCAL_URL
-const UAT = process.env.NEXT_PUBLIC_CCC_UAT_URL
-
 export default function Home() {
-	const [url, setUrl] = useState<string | null | undefined>(LOCAL)
 	return (
 		<main className='w-screen h-full min-h-screen bg-black flex flex-col gap-4 sm:flex-row'>
-			<div className='w-full sm:w-1/2 ring-white  h-full flex flex-col p-4 sm:p-24 gap-12 text-white'>
+			<div className='w-full ring-white  h-full flex flex-col p-4 sm:p-24 gap-12 text-white'>
 				<h1 className='text-4xl '>
-					Why not try an amazing College Costs Calculator?
+					Learning how to publish and embed a component from using npm?
 				</h1>
-				<div className='flex flex-col sm:flex-row h-fit gap-4 sm:gap-12 text-2xl'>
-					<button
-						className='border-2 p-4 rounded-lg hover:text-black hover:bg-white'
-						style={{
-							backgroundColor: url === LOCAL ? 'white' : 'inherit',
-							color: url === LOCAL ? 'black' : 'inherit',
-						}}
-						onClick={() => setUrl(LOCAL)}
-					>
-						LOCALHOST
-					</button>
-					<button
-						className='border-2 p-4 rounded-lg hover:text-black hover:bg-white'
-						onClick={() => setUrl(UAT)}
-						style={{
-							backgroundColor: url === UAT ? 'white' : 'inherit',
-							color: url === UAT ? 'black' : 'inherit',
-						}}
-					>
-						UAT
-					</button>
-				</div>
+				<section className='p-23 ring-2 w-full h-fit min-h-[500px] grid grid-cols-2 items-center justify-center text-center text-5xl'>
+					<Badge />
+					<Button />
+				</section>
 			</div>
-			{url && (
-				<iframe
-					className='flex w-full items-center justify-center px-4 sm:w-1/2 sm:mt-12'
-					id='inlineIframeTest'
-					title='Use the College Costs Calculator'
-					height='880'
-					src={url}
-				/>
-			)}
 		</main>
 	)
 }
